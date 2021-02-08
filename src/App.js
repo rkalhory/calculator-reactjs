d70533e1ef1c00cfcb59e7e483530338317de159
roya kalhory <rkalhory@gmail.com>
2021-02-08 20:04:12 +0330

    v.1.1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import Button from "./components/Button";
 import Input from "./components/Input";
 import * as math from 'mathjs'
-import {ClearBtn} from "./components/ClearBtn";
+import {string} from "mathjs";
 
 class App extends React.Component {
     constructor(propse) {
@@ -14,41 +14,45 @@ class App extends React.Component {
         }
     }
     checkchar = (val) =>{
-        console.log('checkchar')
-        if (val === "."||val==="/"||val==="*"||val==="-"||val==="+"||val==="Enter"){
-            console.log('if')
+        if (val === "."||val==="/"||val==="*"||val==="-"||val==="+"){
             if (val === "."){
-                this.setState((s)=>({point:true}))
+                !(this.state.point) &&
+                this.setState((s)=>({point:true}),()=>{this.setState({input: this.state.input + val})})
             }else {
-                this.setState((s)=>({point: false}))
+                this.setState((s)=>({point: false}),()=>{this.setState({input: this.state.input + val})})
             }
-            console.log(this.state.point)
-            this.addinput(val)
-        }else{
-            console.log('else')
-            this.addinput(val)
         }
-
-    }
-    addinput = (val) =>{
-        console.log('addinput')
-        if (!(val==="."&& this.state.point)){
-            console.log('addinput if')
+        else{
             this.setState({input: this.state.input + val})
         }
     }
 
     handleEqual = () => {
-        this.setState({ input: math.evaluate(this.state.input) });
+        const str = this.state.input;
+        // console.log(this.state.input+"       "+str.substring(str.length - 1)+"     "+str.substring(0, str.length - 1));
+        const last=str.substring(str.length - 1)
+        if (last === "."||last==="/"||last==="*"||last==="-"||last==="+"){
+            let g=str.substring(0, str.length - 1)
+           this.setState({input:g},()=>{
+               this.setState(()=>({ input: math.evaluate(this.state.input) }))
+               if(string(this.state.input).includes(".")){this.setState(()=>({point:true}))}
+           })
+        }else{
+            this.setState(()=>({ input: math.evaluate(this.state.input) }))
+            if(string(this.state.input).includes(".")){this.setState(()=>({point:true}))}
+        }
     };
-
 
     getKey = (e) => {
         if (((!isNaN( e.key)) ||  e.key === "."||  e.key === "Enter" ||  e.key === "="||  e.key === "+"||  e.key === "-"||  e.key === "*"||  e.key === "/")){
             e.key==='Enter' ? this.handleEqual() : this.checkchar(e.key)
         }
-
     };
+
+    handleClear = () =>{
+        this.setState(()=>({input:""}))
+        this.setState(()=>({point:false}))
+    }
 
     componentDidMount(){
         document.addEventListener("keydown", this.getKey);
@@ -64,8 +68,8 @@ class App extends React.Component {
                     <p id='title'>my calculator</p>
                     <Input input={this.state.input}/>
                     <div className='row'>
-                        <Button handleClick={this.checkchar}>AC</Button>
-                        <Button handleClick={this.checkchar}>CE</Button>
+                        <Button handleClick={this.handleClear}>AC</Button>
+                        <Button handleClick={this.handleClear}>CE</Button>
                         <Button handleClick={this.checkchar}>%</Button>
                         <Button handleClick={this.checkchar}>/</Button>
                     </div>
@@ -92,11 +96,6 @@ class App extends React.Component {
                         <Button handleClick={this.checkchar}>0</Button>
                         <Button handleClick={() => this.handleEqual()}>=</Button>
                     </div>
-                    {/*<div className="row">*/}
-                    {/*    <ClearBtn handleClear={() => this.setState({ input: "" })}>*/}
-                    {/*        Clear*/}
-                    {/*    </ClearBtn>*/}
-                    {/*</div>*/}
                 </div>
             </div>
         );
